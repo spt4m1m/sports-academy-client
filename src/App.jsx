57fc5ed7db9840b8ca4b98ddbff1962bd797335c
spt4m1m <white.hat.tamim@gmail.com>
2023-06-06 23:53:09 +0600

    project init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl text-center font-bold underline">
+        Sports Academy Project init
+      </h1>
+    </>
+  )
+}
+
+export default App
