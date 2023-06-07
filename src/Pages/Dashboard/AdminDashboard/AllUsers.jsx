12ab6d959e4faea5c73f2b01e5213765fda9fff7
spt4m1m <white.hat.tamim@gmail.com>
2023-06-07 17:27:15 +0600

    make or remove instructor and complete manageUsers

--- src/Pages/Dashboard/AdminDashboard/AllUsers.jsx
+++ src/Pages/Dashboard/AdminDashboard/AllUsers.jsx
@@ -54,6 +54,40 @@ const AllUsers = ({ user, index, refetch }) => {
                 })
         }
     }
+
+
+    // make  instructor 
+    const makeInstructor = (user) => {
+        const sure = window.confirm(`are you sure ? you want to make instructor ${user.name}`);
+        if (sure) {
+            fetch(`${import.meta.env.VITE_APP_API_URL}/users/instructor/${user.email}`, {
+                method: "PUT"
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.modifiedCount > 0) {
+                        toast.success('make instructor success')
+                        refetch()
+                    }
+                })
+        }
+    }
+    // remove  instructor 
+    const removeInstructor = (user) => {
+        const sure = window.confirm(`are you sure ? you want to remove instructor ${user.name}`);
+        if (sure) {
+            fetch(`${import.meta.env.VITE_APP_API_URL}/users/removeinstructor/${user.email}`, {
+                method: "PUT"
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.modifiedCount > 0) {
+                        toast.success('remove instructor success')
+                        refetch()
+                    }
+                })
+        }
+    }
     return (
         <tr>
             <td>{index + 1}</td>
@@ -71,9 +105,9 @@ const AllUsers = ({ user, index, refetch }) => {
                 </div>
             </td>
             <td>
-                <button className="btn bg-red-700 text-white btn-xs">make instructor</button>
+                {user.role == "instructor" ? <button onClick={() => removeInstructor(user)} className="btn bg-red-700 text-white btn-xs">remove instructor</button> : <button onClick={() => makeInstructor(user)} className="btn bg-green-700 text-white btn-xs">make instructor</button>}
             </td>
-            <td>{user.role == "admin" ? <button onClick={() => removeAdmin(user)} className="btn bg-red-700 text-white btn-xs">remove admin</button> : <button onClick={() => makeAdmin(user)} className="btn bg-red-700 text-white btn-xs">make admin</button>}</td>
+            <td>{user.role == "admin" ? <button onClick={() => removeAdmin(user)} className="btn bg-red-700 text-white btn-xs">remove admin</button> : <button onClick={() => makeAdmin(user)} className="btn bg-green-700 text-white btn-xs">make admin</button>}</td>
             <th>
                 <button onClick={() => deleteUser(user)} className="btn bg-red-700 text-white btn-xs">Delete</button>
             </th>
