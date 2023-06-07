9d2035d3202f417c8896c764c6e9d08440d6fa14
spt4m1m <white.hat.tamim@gmail.com>
2023-06-08 04:38:09 +0600

    approve class implement

--- src/Pages/Dashboard/AdminDashboard/ManageClassRow.jsx
+++ src/Pages/Dashboard/AdminDashboard/ManageClassRow.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
 import { Icon } from '@iconify/react';
+import { toast } from 'react-hot-toast';
 
-const ManageClassRow = ({ singleClass, index }) => {
-    const { classimg, classname, instructorname, instructoremail, availableseat, price, status } = singleClass;
+const ManageClassRow = ({ singleClass, index, refetch }) => {
+    const { classimg, classname, instructorname, instructoremail, availableseat, price, status, _id } = singleClass;
+
+    const approveClass = (id) => {
+        const sure = window.confirm('approve this calss?');
+        if (sure) {
+            fetch(`${import.meta.env.VITE_APP_API_URL}/classes/${id}?status=approved`, {
+                method: "PUT",
+                headers: {
+                    authorization: `Bearer ${localStorage.getItem('access-token')}`
+                }
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.result.modifiedCount > 0) {
+                        toast.success('class approved')
+                        refetch();
+                    }
+                })
+        }
+    };
+
     return (
         <tr>
             <th>{index + 1}</th>
@@ -19,8 +40,8 @@ const ManageClassRow = ({ singleClass, index }) => {
             <td className='flex items-center'><Icon className='text-green-500' icon="material-symbols:circle" />{status}</td>
             <td>
                 <div>
-                    <td><button className='btn btn-primary btn-outline btn-xs'>Approve</button></td>
-                    <td><button className='btn bg-red-600 normal-case text-white btn-xs'>Deny</button></td>
+                    <td><button disabled={status == 'deny'} onClick={() => approveClass(_id)} className='btn bg-green-600 normal-case text-white btn-xs'>Approve</button></td>
+                    <td><button disabled={status == "approved"} className='btn bg-red-600 normal-case text-white btn-xs'>Deny</button></td>
                     <td><button className='btn btn-primary btn-outline btn-xs'>Feedback</button></td>
                 </div>
             </td>
