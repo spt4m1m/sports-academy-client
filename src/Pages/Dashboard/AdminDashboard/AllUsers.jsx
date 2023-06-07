cdd4bcd34e6fea88d8d79540cf0ea6039cc979ec
spt4m1m <white.hat.tamim@gmail.com>
2023-06-07 16:53:57 +0600

    make admin or remove admin

--- src/Pages/Dashboard/AdminDashboard/AllUsers.jsx
+++ src/Pages/Dashboard/AdminDashboard/AllUsers.jsx
@@ -3,6 +3,8 @@ import { toast } from 'react-hot-toast';
 
 const AllUsers = ({ user, index, refetch }) => {
     const { name, email } = user;
+
+    // delete user 
     const deleteUser = (user) => {
         const sure = window.confirm(`are you sure ? you want to delete ${user.name}`);
         if (sure) {
@@ -13,6 +15,40 @@ const AllUsers = ({ user, index, refetch }) => {
                 .then(data => {
                     if (data.deletedCount > 0) {
                         toast.success('delete success')
+                        refetch()
+                    }
+                })
+        }
+    };
+
+    // make admin 
+    const makeAdmin = (user) => {
+        const sure = window.confirm(`are you sure ? you want to make admin ${user.name}`);
+        if (sure) {
+            fetch(`${import.meta.env.VITE_APP_API_URL}/users/admin/${user.email}`, {
+                method: "PUT"
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.modifiedCount > 0) {
+                        toast.success('make admin success')
+                        refetch()
+                    }
+                })
+        }
+    }
+
+    // remove admin 
+    const removeAdmin = (user) => {
+        const sure = window.confirm(`are you sure ? you want to remove admin ${user.name}`);
+        if (sure) {
+            fetch(`${import.meta.env.VITE_APP_API_URL}/users/removeadmin/${user.email}`, {
+                method: "PUT"
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.modifiedCount > 0) {
+                        toast.success('remove admin success')
                         refetch()
                     }
                 })
@@ -37,7 +73,7 @@ const AllUsers = ({ user, index, refetch }) => {
             <td>
                 <button className="btn bg-red-700 text-white btn-xs">make instructor</button>
             </td>
-            <td><button className="btn bg-red-700 text-white btn-xs">make admin</button></td>
+            <td>{user.role == "admin" ? <button onClick={() => removeAdmin(user)} className="btn bg-red-700 text-white btn-xs">remove admin</button> : <button onClick={() => makeAdmin(user)} className="btn bg-red-700 text-white btn-xs">make admin</button>}</td>
             <th>
                 <button onClick={() => deleteUser(user)} className="btn bg-red-700 text-white btn-xs">Delete</button>
             </th>
