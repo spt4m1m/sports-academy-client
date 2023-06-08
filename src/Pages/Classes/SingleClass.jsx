0ff3dcd9ccd6f41ddac0fbb516136465e8f267c3
spt4m1m <white.hat.tamim@gmail.com>
2023-06-08 23:36:58 +0600

    select class by student and send class in db

--- src/Pages/Classes/SingleClass.jsx
+++ src/Pages/Classes/SingleClass.jsx
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { AuthContext } from '../../Providers/AuthProvider';
+import Swal from 'sweetalert2';
 
 const SingleClass = ({ singleClass, refetch }) => {
+    const [isSelected, setIsSelected] = useState(false)
+    const { user } = useContext(AuthContext);
     const { classimg, classname, instructorname, instructoremail, availableseat, price } = singleClass;
+    const handleSelect = (classData) => {
+        // console.log('selec class', classData);
+        const { classname, availableseat, price } = classData;
+        const { displayName, email } = user;
+        const selectedClassData = {
+            classname,
+            availableseat,
+            price,
+            studentname: displayName,
+            studentemail: email,
+        }
+        fetch(`${import.meta.env.VITE_APP_API_URL}/selectedclass`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                authorization: `Bearer ${localStorage.getItem('access-token')}`
+            },
+            body: JSON.stringify(selectedClassData),
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.result.insertedId) {
+                    Swal.fire({
+                        title: "Class Selected!!",
+                        icon: "success"
+                    })
+                    setIsSelected(true)
+                }
+            })
+    }
     return (
         <div className="card w-96 bg-neutral gap-5 m-5 shadow-xl mx-auto text-white">
             <figure><img src={classimg} alt="class img" /></figure>
@@ -12,7 +46,9 @@ const SingleClass = ({ singleClass, refetch }) => {
                 <p>Available Seats : {availableseat}</p>
                 <p>Price : {price}Tk.</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Select</button>
+                    {
+                        isSelected ? <span className='bg-green-500 p-2 rounded-md'>Class Selected</span> : <button onClick={() => handleSelect(singleClass)} className="btn btn-primary">Select Class</button>
+                    }
                 </div>
             </div>
         </div>
