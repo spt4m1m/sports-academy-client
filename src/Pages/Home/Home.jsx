f2abab7b9b9140ba3adef6801841a602afbfabb8
spt4m1m <white.hat.tamim@gmail.com>
2023-06-10 01:49:33 +0600

    added extra section in home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import Banner from './Banner';
 import PopularClasses from './PopularClasses';
 import PopularInstructors from './PopularInstructors';
+import Activities from './Activities';
 
 const Home = () => {
     return (
         <div>
             <Banner />
             <PopularClasses />
+            {/* extra section */}
+            <Activities />
             <PopularInstructors />
         </div>
     );
