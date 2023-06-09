4af444da12483e627e6c2fdb18e854dfbd91063e
spt4m1m <white.hat.tamim@gmail.com>
2023-06-10 00:44:30 +0600

    added popular class section in homepage

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from './Banner';
+import PopularClasses from './PopularClasses';
 
 const Home = () => {
     return (
         <div>
             <Banner />
+            <PopularClasses />
         </div>
     );
 };
---
+++ src/Pages/Home/PopularClasses.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import useAxiosSecure from '../../hooks/useAxiosSecure';
+import { useQuery } from 'react-query';
+import Loader from '../../Components/Loader/Loader';
+import SingleClass from '../Classes/SingleClass';
+
+const PopularClasses = () => {
+    const [axiosSecure] = useAxiosSecure()
+    const { isLoading, data: classes } = useQuery({
+        queryKey: ['classes'],
+        queryFn: async () => {
+            const res = await axiosSecure(`/popularclasses`);
+            return res.data;
+        }
+    }
+    );
+    if (isLoading) {
+        return <Loader />
+    }
+    return (
+        <div>
+            <h1 className='text-primary text-xl md:text-4xl text-center my-5'>Popular  Classes</h1>
+            <div className='grid md:grid-cols-3'>
+                {
+                    classes.slice(0, 6).map(singleClass => <SingleClass key={singleClass._id} singleClass={singleClass} />)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default PopularClasses;
