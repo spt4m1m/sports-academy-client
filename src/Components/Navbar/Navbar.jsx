db95dba1e63805b15993f7e3fc09854739a85d6b
spt4m1m <white.hat.tamim@gmail.com>
2023-06-10 14:58:27 +0600

    added top scrollbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -52,7 +52,7 @@ const Navbar = () => {
         }
     </>
     return (
-        <div className="navbar bg-neutral-focus">
+        <div className="navbar bg-neutral-focus fixed top-0 left-0 z-10">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
--- src/MainLayout.jsx
+++ src/MainLayout.jsx
@@ -2,12 +2,19 @@ import { Outlet } from "react-router-dom"
 import Navbar from "./Components/Navbar/Navbar"
 import Footer from "./Components/Footer/Footer"
 import { Toaster } from "react-hot-toast"
+import { motion, useScroll } from "framer-motion";
+import './motion.css'
 
 
 function App() {
+  const { scrollYProgress } = useScroll();
 
   return (
     <>
+      <motion.div
+        className="progress-bar"
+        style={{ scaleX: scrollYProgress }}
+      />
       <Navbar />
       <Outlet />
       <Toaster />
