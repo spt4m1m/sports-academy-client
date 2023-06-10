3f196db620ce3d212f369710d04f35f77895f445
spt4m1m <white.hat.tamim@gmail.com>
2023-06-11 01:44:31 +0600

    added user profile

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Fade } from 'react-awesome-reveal';
+import { AuthContext } from '../../Providers/AuthProvider';
 
 const Profile = () => {
+    const { user } = useContext(AuthContext)
     return (
         <Fade delay={500} cascade duration={1000}>
             <div className='mt-12'>
-                <h1 className='text-center text-3xl py-10 text-primary'>This is Profile Page</h1>
+                <h1 className='text-center text-3xl py-10 text-primary mt-5'>This is Your Profile {user.displayName}</h1>
+
+                <div className="card card-side bg-neutral my-10 w-1/2 mx-auto shadow-xl">
+                    <figure><img className='w-80' src={user.photoURL} alt="Movie" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title"> Name : <span className='text-primary'>{user.displayName}</span></h2>
+                        <h2 className="card-title"> Name : <span className='text-primary'>{user.email}</span></h2>
+                    </div>
+                </div>
             </div>
         </Fade>
     );
